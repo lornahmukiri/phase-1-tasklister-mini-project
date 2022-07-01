89a66feb94885dcec54534e74bc1fdef82214f38
Lornah Mukiri <mukirikabuli@gmail.com>
2022-07-01 15:18:12 +0300

    Completed lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+  let form = document.querySelector("form")
+  form.addEventListener("submit", (e) =>{
+    e.preventDefault()
+    buildTodo(e.target.newTask.value)
+    form.reset()
+  })
 });
+
+function buildTodo(todo) {
+  const li = document.createElement('li')
+  const btn = document.createElement('button')
+  btn.textContent = 'x'
+  li.textContent = `${todo}`
+  li.appendChild(btn)
+  document.getElementById('tasks').appendChild(li)
+
+  btn.addEventListener(click, (e) => e.target.parentNode.remove())
+}
